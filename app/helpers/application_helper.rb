8a3114734085ca68d6e9f61163b1a800290ec5d8
Polo Ornelas <poloornelas@yahoo.com>
2012-03-02 13:47:27 -0600

    added navbar partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,7 @@
 module ApplicationHelper
 	def bootstrap_menu_entry(title, path)
-		#	          <li><%= link_to "About", about_path%></li>
 		active = ""
-		active="class='active' " if (path == request.path)
+		active = "class='active' " if (path == request.path)
 		"<li " + active + ">" + link_to(title, path) + "</li>"
 	end
 	
